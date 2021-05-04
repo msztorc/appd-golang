e28747d93f245e8b8f6b97ff18c8f077684715f3
Mirosław Sztorc <msztorc@users.noreply.github.com>
2021-05-04 16:47:06 +0200

    Update main.go

--- main.go
+++ main.go
@@ -7,17 +7,17 @@ import (
 )
 
 func homePage(w http.ResponseWriter, r *http.Request){
-	btHandle := appd.StartBT("/", "")
+    btHandle := appd.StartBT("/", "")
     fmt.Fprintf(w, "Welcome to the home page!")
-	fmt.Println("Endpoint hit: /")
-	appd.EndBT(btHandle)
+    fmt.Println("Endpoint hit: /")
+    appd.EndBT(btHandle)
 }
 
 func homePage1(w http.ResponseWriter, r *http.Request){
-	btHandle := appd.StartBT("/hello", "")
+    btHandle := appd.StartBT("/hello", "")
     fmt.Fprintf(w, "Hello world!")
-	fmt.Println("Endpoint hit: /hello")
-	appd.EndBT(btHandle)
+    fmt.Println("Endpoint hit: /hello")
+    appd.EndBT(btHandle)
 }
 
 func handleRequests() {
@@ -28,29 +28,29 @@ func handleRequests() {
 
 func main() {
     cfg := appd.Config{}
- 	// Appdynamics configuration
-	cfg.Controller.Host = ""
-	cfg.Controller.Port = 8090
-	cfg.Controller.UseSSL = false
-	cfg.Controller.Account = "customer1"
-	cfg.Controller.AccessKey = ""
+    // Appdynamics configuration
+    cfg.Controller.Host = ""
+    cfg.Controller.Port = 8090
+    cfg.Controller.UseSSL = false
+    cfg.Controller.Account = "customer1"
+    cfg.Controller.AccessKey = ""
 
-	// App Context
-	cfg.AppName = "go-app"
-	cfg.TierName = "go-tier"
-	cfg.NodeName = "go-node"
+    // App Context
+    cfg.AppName = "go-app"
+    cfg.TierName = "go-tier"
+    cfg.NodeName = "go-node"
 
-	// misc
-	cfg.InitTimeoutMs = 5000
-	cfg.Logging.MinimumLevel =appd.APPD_LOG_LEVEL_TRACE
-	
+    // misc
+    cfg.InitTimeoutMs = 5000
+    cfg.Logging.MinimumLevel =appd.APPD_LOG_LEVEL_TRACE
 
-	// init the SDK
-	if err := appd.InitSDK(&cfg); err != nil {
-		fmt.Printf("Error initializing the AppDynamics SDK\n")
-	} else {
-		fmt.Printf("Initialized AppDynamics SDK successfully\n")
-	}
-     
-	 handleRequests()
+
+    // init the SDK
+    if err := appd.InitSDK(&cfg); err != nil {
+	fmt.Printf("Error initializing the AppDynamics SDK\n")
+    } else {
+	fmt.Printf("Initialized AppDynamics SDK successfully\n")
+    }
+
+    handleRequests()
 }
